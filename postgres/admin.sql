1c60c0401f32f0b8fab19140170057c41f8bf81f
some0necoding <codingandprogramming.code@gmail.com>
2024-07-14 11:39:18 +0200

    adding default bibliotecario to database

---
+++ postgres/admin.sql
@@ -0,0 +1,10 @@
+-- Inserisce l'utente amministratore
+--
+-- Email:    admin@biblioteca.org
+-- Password: admin
+--
+INSERT INTO biblioteca.bibliotecario (email, hash)
+VALUES (
+    'admin@biblioteca.org',
+    '$2y$10$WpQPcrKRbmwl3cJA1Sx0p.Au63M.ycTp5vWT9sqC5gxobrI/UQasq'
+);

1ea62b2d73b12c2c46e331c972335ab23872d362
some0necoding <codingandprogramming.code@gmail.com>
2024-07-17 12:52:22 +0200

    fix[app]: rewriting logout function

--- app/src/backend/backend.php
+++ app/src/backend/backend.php
@@ -1430,8 +1430,8 @@
      * 'user_email'.
      */
     function logout(): void {
-        unset($_SESSION['user_id']);
-        unset($_SESSION['user_email']);
+        session_unset();
+        session_destroy();
     }
 
 ?>

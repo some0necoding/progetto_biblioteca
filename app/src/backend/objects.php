69fb57f0a832a11fabd7fa1c474e0cec61a1d335
some0necoding <codingandprogramming.code@gmail.com>
2024-07-17 11:50:49 +0200

    fix[app]: removing type declaration from class constants which are illegal

--- app/src/backend/objects.php
+++ app/src/backend/objects.php
@@ -3,10 +3,10 @@
 
     class Isbn {
         private string $isbn;
-        private const string PATTERN = '[0-9]{13}';
+        private const PATTERN = '[0-9]{13}';
 
         public function __construct(string $isbn) {
-            if (!preg_match('/' . PATTERN . '/', $isbn))
+            if (!preg_match('/' . self::PATTERN . '/', $isbn))
                 throw new InvalidIsbnException($isbn);
             $this->isbn = $isbn;
         }
@@ -22,12 +22,12 @@
 
     class Email {
         private string $email;
-        private const string PATTERN = <<<'regex'
+        private const PATTERN = <<<'regex'
             (?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])
             regex;
 
         public function __construct(string $email) {
-            if (!preg_match('/' . PATTERN . '/', $email))
+            if (!preg_match('<' . self::PATTERN . '>', $email))
                 throw new InvalidEmailException($email);
             $this->email = $email;
         }
@@ -67,7 +67,7 @@
             regex;
 
         public function __construct(string $codice_fiscale) {
-            if (!preg_match('/' . PATTERN . '/i', $codice_fiscale))
+            if (!preg_match('/' . self::PATTERN . '/i', $codice_fiscale))
                 throw new InvalidCodiceFiscaleException($codice_fiscale);
             $this->codice_fiscale = $codice_fiscale;
         }

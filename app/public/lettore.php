d6a3b8bf8d0d6d66029db3b9687832cca45ec891
some0necoding <codingandprogramming.code@gmail.com>
2024-07-18 16:14:53 +0200

    fix[app]: refactored lettore and bibliotecario pages + parametrizing operation execution

--- app/public/lettore.php
+++ app/public/lettore.php
@@ -1,21 +1,57 @@
 <?php
-    include '../src/utils.php';
-    include '../src/backend.php';
-
-    session_start();
+    include_once '../src/frontend/utils.php';
+    include_once '../src/backend/backend.php';
+    include_once '../src/frontend/operazioni.php';
 
     $errors = [];
     $inputs = $_POST;
 
     $tab = $_GET['tab'] ?? 'libri';
 
-    controllaPermessiOReindirizza(Utente::LETTORE, $inputs, $errors, 'login.php');
+    if (!isLoggedIn(Utente::LETTORE))
+        redirect_to('login.php');
 
     if (post()) {
-        $inputs['operazione'] = Operazione::from($inputs['operazione']);
-        switch($inputs['operazione']) {
-            case Operazione::RICHIEDI_PRESTITO:
+        $tipoOperazione = TipoOperazione::from($inputs['operazione'])->value;
+        unset($inputs['operazione']);
+
+        $classeOperazione = OPERAZIONI[$tipoOperazione];
+        $operazione = new $classeOperazione($inputs);
+
+        try {
+            $errors = $operazione->esegui();
+
+            foreach ($errors as $key => $value) {
+                $errors[$tipoOperazione][$key] = $value;
+                unset($errors[$key]);
+            }
+
+            if (empty($errors))
+                $errors[$tipoOperazione] = [];
+
+            foreach ($inputs as $key => $value) {
+                $inputs[$tipoOperazione][$key] = $value;
+                unset($inputs[$key]);
+            }
+
+            if (empty($inputs))
+                $inputs[$tipoOperazione] = [];
+
+            if (!empty($errors)) {
+                redirect_with('lettore.php?tab=' . $tab, [
+                    'inputs' => $inputs,
+                    'errors' => $errors
+                ]);
+            }
+            if ($tipoOperazione === TipoOperazione::ELIMINA_ACCOUNT_LETTORE->value ||
+                $tipoOperazione === TipoOperazione::LOGOUT->value)
+            {
+                redirect_to('login.php');
+            }
+        } catch (ErroreInternoDatabaseException $e) {
+            redirect_to('internal_error.php');
         }
+
     } else if (get()) {
         [$inputs, $errors] = session_get('inputs', 'errors');
     }
@@ -23,10 +59,10 @@
 ?>
 
 <?php view('header', ['title' => 'Lettore']); ?>
-<?php view('lettore/lettore_navbar', [ 'active' => $tab, 'page' => 'lettore.php' ]); ?>
+<?php view('lettore/navbar', [ 'active' => $tab, 'page' => 'lettore.php' ]); ?>
 <div class="container">
     <div class="row">
-        <div class="col mt-4">
+        <div class="col mt-4 p-0">
             <?php 
                 view('lettore/' . $tab, [
                     'inputs' => $inputs,

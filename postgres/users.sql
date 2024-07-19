4c0351628d4d3d8936f57e758d8319c459da03ee
some0necoding <codingandprogramming.code@gmail.com>
2024-07-19 18:28:09 +0200

    feat[postgres]: adding a default lettore account + renaming file

--- postgres/users.sql
+++ postgres/users.sql
@@ -8,3 +8,14 @@ VALUES (
     'admin@biblioteca.org',
     '$2y$10$WpQPcrKRbmwl3cJA1Sx0p.Au63M.ycTp5vWT9sqC5gxobrI/UQasq'
 );
+
+-- Inserisce un lettore
+--
+-- Email: lettore@biblioteca.org
+-- Password: lettore
+--
+INSERT INTO biblioteca.lettore (email, hash)
+VALUES (
+    'lettore@biblioteca.org',
+    '$2y$10$x0v8KhLiEa/2ZYXCsFG.9enXcjZaJBWsrb/CMZ.d./OpT6t4cWGb6'
+);
